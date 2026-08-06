66c79c49b542713179945acb07b88769d9de4133
afan-sdt <affaansaadat@gmail.com>
2026-08-06 12:43:25 -0400

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        #connected and no cycles,  do a DFS from a node and see if you can reach every node
+        # how do we check if connected? keep track of visited, if all nodes visited, connected
+        # for cycle, dfs + parent tracking, if it's visited and not parent, cycle
+        
+        if len(edges) != n-1 : # if more, cycle. if less, not connected
+            return False
+        adj = {i:[] for i in range(n)}
+        for i,j in edges:
+            adj[i].append(j)
+            adj[j].append(i)
+        visited = set()
+        
+        def dfs(node: int, parent: int) -> bool:
+            if node in visited and node == parent:
+                return True
+            visited.add(node)
+            for i in adj[node]:
+                if i in visited and i == parent: # need to skip neighbor situation
+                    continue
+                if i in visited and i!= parent: # but need to report cycle
+                    return False
+                if not dfs(i, node):
+                    return False
+            return True
+        
+        if not dfs(0, -1):
+            return False
+        return len(visited) == n
+
