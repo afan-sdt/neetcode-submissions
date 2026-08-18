f2d2e4db044763d261fa6bb301c52f90b27c9cf6
afan-sdt <affaansaadat@gmail.com>
2026-08-18 14:59:12 -0400

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        # construct a counts dict
+        counts = {}
+        for i in s:
+            counts[i] = 1 + counts.get(i, 0)
+        # have a curr letter counts which represents the letters in the current window
+        curr = {}
+        #when this curr dict is empty, we add the current length to res array
+        currCount = 0
+        res = []
+        for ch in s:
+            print(ch)
+            print(curr)
+            print(currCount)
+            if ch in curr:
+                curr[ch] -= 1
+                if curr[ch] == 0:
+                    curr.pop(ch)
+            elif ch not in curr:
+                curr[ch] = counts[ch] - 1
+                if curr[ch] == 0:
+                    curr.pop(ch)
+                counts.pop(ch)
+            currCount+=1
+            if len(curr) == 0:
+                res.append(currCount)
+                currCount = 0
+        print(res)
+        return res
