086d37f7eaf00be1f0e2c85122d24c5cfa645026
afan-sdt <affaansaadat@gmail.com>
2026-09-13 01:42:07 -0400

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        #sort by position then we see if arrival time of a <= arrival time of b
+        # t = 10
+        # 0, 1, 4, 7  position
+        # 1, 2, 2, 1 speed
+        # 10s 5s 3s 3s
+        # 
+        # go in reversed order + add eta to stack
+        # if the current end time is > top of stack, append it, otherwise ignore 
+        # this is because if the end time is <=, it will arrive as part of the previous fleet
+        #               | <-target
+        # car car car 
+        tups = list(zip(position, speed))
+        tups.sort(reverse=True)
+        res = len(position)
+        n = len(position)
+        stck = []
+        for p , s in tups:
+            endTime = (target - p) / s
+            if stck and endTime <= stck[-1]:
+                continue
+            stck.append(endTime) 
+        return len(stck)
